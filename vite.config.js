b1118c9e92e45fdfcbb122939eb4da8543fd2cc2
AuriKosmos <aurikosmos@gmail.com>
2026-08-05 16:49:28 -0500

    Corrige base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Cambia 'auri-kosmos' por el nombre exacto de tu repositorio en GitHub
-// si algún día lo renombras, para que las rutas de GitHub Pages sigan funcionando.
 export default defineConfig({
-  base: '/auri-kosmos/',
+  base: '/aurikosmos/',
   plugins: [react()],
 })
